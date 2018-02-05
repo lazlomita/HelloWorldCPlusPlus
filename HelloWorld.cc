b1508d9fc109f2c08e177993cd8d5e7787b911ca
lazlo.mita <lazlo.mita@gmail.com>
2018-02-05 09:56:46 -0400

    Initial code HelloWorld

---
+++ HelloWorld.cc
@@ -0,0 +1,14 @@
+//Print Hello world in C++ programming language
+/******************************************************************************
+ *  Compilation:  g++ HelloWorld.cc -o HelloWorld
+ *  Execution:    HelloWorld
+ ******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	// Prints "Hello world" to the terminal window.
+    cout << "Hello World C++\n";
+    return 0;
+}
