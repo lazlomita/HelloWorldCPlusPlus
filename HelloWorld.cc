5de8ebb646d0050858375eabdcbf2ddcc3757cf8
lazlo.mita <lazlo.mita@gmail.com>
2018-02-13 15:48:11 -0400

    Add FOR, IF, ELSE, Concatenation.

--- HelloWorld.cc
+++ HelloWorld.cc
@@ -1,14 +1,26 @@
-//Print Hello world in C++ programming language
 /******************************************************************************
  *  Compilation:  g++ HelloWorld.cc -o HelloWorld
  *  Execution:    HelloWorld
+ *  
+ *  Print Hello world in C++ programming language
+ *
  ******************************************************************************/
 #include <iostream>
 using namespace std;
 
-int main() 
-{
+int main () {
 	// Prints "Hello world" to the terminal window.
-    cout << "Hello World C++\n";
-    return 0;
+	cout << "Hello World C++" << endl << endl;
+	// Run a cycle that goes from 1 to 10
+	for( int i = 1; i <= 10; i = i + 1 ) {
+		// Decide if the number is even or not
+		if( i%2 == 0 ) {
+			// Text concatenation and print
+			cout << "Count is: " << i << " and it is even." << endl;
+		} else {
+			// Text concatenation and print
+			cout << "Count is: " << i << " and it is odd." << endl;
+		}
+	}
+	return 0;
 }
